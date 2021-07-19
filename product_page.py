9be7d5f8b080e2066cd327007334cbfc41c5d8aa
Alpha <mavamalonga.alpha@gmail.com>
2021-07-19 20:25:13 +0200

    success all extract value

--- product_page.py
+++ product_page.py
@@ -5,21 +5,23 @@ import csv
 """
 product_page_url #
 universal_ product_code (upc) #
-title
+title #
 price_including_tax #
 price_excluding_tax #
 number_available #
-product_description
-category
+product_description #
+category #
 review_rating #
-image_ur
+image_url #
 """
 
-product_page_url = "http://books.toscrape.com/catalogue/night-shift-night-shift-1-20_335/index.html"
-page = requests.get(product_page_url)
-soup = BeautifulSoup(page.content, 'html.parser')
+def get_table_information():
 
-def get_table_informations():
+	product_page_url = "http://books.toscrape.com/catalogue/night-shift-night-shift-1-20_335/index.html"
+	page = requests.get(product_page_url)
+	soup = BeautifulSoup(page.content, 'html.parser')
+
+
 	table_striped = soup.find_all("table", class_="table table-striped")
 	tds = table_striped[0].find_all("td")
 
@@ -28,6 +30,29 @@ def get_table_informations():
 	price_excluding_tax = tds[2].string
 	number_available = tds[5].string
 	review_rating = tds[-1].string
+
+
+def get_header_information():
+	product_page_url = "http://books.toscrape.com/catalogue/night-shift-night-shift-1-20_335/index.html"
+	page = requests.get(product_page_url)
+	soup = BeautifulSoup(page.content, 'html.parser')
+
+	title = soup.find("title").string
+	balise_product_description = soup.find(attrs={"name":"description"})
+	product_decription = balise_product_description['content']
+
+	div_product_gallery = soup.find("div", id="product_gallery")
+	image_url = div_product_gallery.find("img")["src"]
+
+
+	block_ul = soup.find("ul", class_="breadcrumb")
+	block_all_a = block_ul.find_all("a")
+	category = block_all_a[-1].string
+	print(category)
+
+
+
+get_header_information()
 
 
 

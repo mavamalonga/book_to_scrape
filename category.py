fa4ef378e91de0f86701d4935b71ba0381fc858f
Alpha <mavamalonga.alpha@gmail.com>
2021-07-28 22:09:36 +0200

    debug collection_name in category file

--- category.py
+++ category.py
@@ -7,49 +7,54 @@ and checks if the category is multi-page and retrieves the other URLs.
 The main method returns the urls list of all the pages in the category."""
 class CATEGORIES:
 
-	 def __init__(self, category_name, page_url):
+	 def __init__(self, category_name, index_page):
 	 	self.category_name = category_name
-	 	self.url_page = page_url
-	 	self.root_url = "http://books.toscrape.com/catalogue/category/books/"
-	 	self.next_page_url = None
-	 	self.urls_list = []
+	 	self.index_page = index_page
+	 	self.root_path = "http://books.toscrape.com/catalogue/category/books/"
+	 	self.pages_list = []
 
-	 def make_request(self):
+	 def make_request(self, page):
 	 	try:
-	 		response = requests.get(self.url_page)
+	 		response = requests.get(page)
 	 		self.soup = BeautifulSoup(response.content, 'html.parser')
-	 		self.urls_list.append(self.url_page)
-	 	except Exception:
-	 		print("Failed request!; ERROR" + str(Exception))
+	 		self.pages_list.append(page)
+	 	except Exception as e:
+	 		error = "Failed request!; ERROR" + str(e)
+	 		print(error)
 	 		return None
 
-	 def parse(self, next_page):
-	 	next_page_url = self.root_url + str(self.category_name) + "/" + str(next_page)
-	 	return next_page_url
+	 def get_collection_value(self):
+	 	self.collection = self.index_page.split("/")
+	 	del self.collection[0:6]
+	 	return self.collection[0]
 
-	 def get_next_page_url(self):
+	 def get_next_page(self, collection_name):
 	 	try:
-	 		next_page = self.soup.find("ul", class_="pager").find("li", class_="next").find("a")["href"]
-	 		if next_page is not None:
-	 			next_page_url = self.parse(next_page)
-	 			return next_page_url
-	 	except Exception:
-	 		return None
+	 		href = self.soup.find("ul", class_="pager").find("li", class_="next").find("a")["href"]
+	 		if href is not None:
+	 			next_page = self.root_path + str(collection_name) + "/" + str(href)
+	 			return next_page
+	 	except Exception as e:
+	 		error = "No next page; ERROR: " + str(e)
+	 		#print(error) 
 
 	 def main(self):
-	 	self.make_request()
-	 	self.next_page_url = self.get_next_page_url()
+	 	self.make_request(self.index_page)
+	 	self.collection_name = self.get_collection_value()
+	 	self.next_page = self.get_next_page(self.collection_name)
 
-	 	while self.next_page_url is not None:
-	 		self.url_page = self.next_page_url
-	 		self.make_request()
-	 		self.next_page_url = self.get_next_page_url()
-	 	print("The are " + str(len(self.urls_list)) + " pages in the " + str(self.category_name) + " category.")
-	 	return self.urls_list
+	 	while self.next_page is not None:
+	 		self.make_request(self.next_page)
+	 		self.next_page = self.get_next_page(self.collection_name)
+
+	 	print("The are " + str(len(self.pages_list)) + " pages in the " + str(self.category_name) + " category.")
+	 	return self.pages_list
 
 
 """
-url = "http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
-categories = CATEGORIES(url)
-categories.main()
+url = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
+category_name = "sequential-art"
+categories = CATEGORIES(category_name, url)
+main = categories.main()
+print(main)
 """
